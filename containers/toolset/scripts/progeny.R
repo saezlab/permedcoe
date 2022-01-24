7544c7afb9cd808428d8dae17e7dd97ef3f582e9
Pablo R. Mier <pablo.rodriguez.mier@gmail.com>
2022-01-24 16:02:51 +0100

    Add progeny script for the toolset

--- containers/toolset/scripts/progeny.R
+++ containers/toolset/scripts/progeny.R
@@ -1,17 +1,21 @@
 #!/usr/bin/env Rscript --vanilla
 
 library(progeny)
-library(tools)
 library(dorothea)
 library(readr)
 library(optparse)
+library(tibble)
+library(dplyr)
 
 parser <- OptionParser(
   usage = "usage: %prog expression_csv_file_or_url output_file [options]",
   option_list = list(
     make_option(c("-o", "--organism"), default="Human", help="Organism (Mouse, Human). Default = Human"),
     make_option(c("-i", "--ntop"), default=100, help="Number of top genes used for estimation of TF activities. Default = 100"),
+    make_option(c("-c", "--col_genes"), default="GENE_SYMBOLS", help="Name of the column containing the gene symbols. Default = GENE_SYMBOLS"),
     make_option(c("-s", "--scale"), default=T, help="Scale the data. Default = TRUE"),
+    make_option(c("-e", "--exclude_cols"), default="GENE_title", help="Exclude columns containing the given string. Default = 'GENE_title'"),
+    make_option(c("-t", "--tsv"), default=F, help="Assume file is TSV instead of CSV. Default = FALSE"),
     make_option(c("-v", "--verbose"), default=F, help="Verbosity (default False)")
   ),
   add_help_option = T,
@@ -28,46 +32,38 @@ if (verbose) {
 }
 
 if (startsWith(file, "http") || startsWith(file, "www.")) {
-    if (verbose) {
-        
-    }
+  url <- file
+  if (verbose) {
+    print("Downloading file...")
+  }
+  file <- tempfile()
+  download.file(url, file, mode = "wb")
 }
 
-if (tolower(tools::file_ext(file)) == "zip") {
-
+if (arguments$options$tsv) {
+  df <- read_tsv(file)  
+} else {
+  df <- read_csv(file)
 }
 
+df <- df[!is.na(df[arguments$options$col_genes]),]
 
-DE_data <- read_csv(arguments$args[1])
-if (verbose) {
-  DE_data
-}
+# Estimate pathway activities with PROGENy
+df_progeny_score <- df %>% 
+  replace(is.na(.), 0) %>% 
+  select(-contains(arguments$options$exclude_cols)) %>%
+  column_to_rownames(var = arguments$options$col_genes) %>%
+  as.matrix() %>%
+  progeny::progeny(., scale=arguments$options$scale, 
+                   organism=arguments$options$organism, 
+                   top = arguments$options$ntop, 
+                   verbose = arguments$options$verbose) %>%
+  as_tibble()
 
-DE_matrix <- DE_data %>% 
-  dplyr::select(arguments$options$id_col, arguments$options$weight_col) %>% 
-  dplyr::filter(!is.na(arguments$options$weight_col)) %>% 
-  column_to_rownames(var = arguments$options$id_col) %>%
-  as.matrix()
-
-dorothea::dorothea_hs
-data(dorothea_hs, package = "dorothea")
-regulons <- dorothea_hs %>%
-  dplyr::filter(confidence %in% unlist(strsplit(arguments$options$confidence,",")))
-
-network <- intersect_regulons(DE_matrix, regulons, tf, target, minsize = 5)
-network$likelihood <- 1
-
-tf_activity <- decoupleR::run_viper(
-  DE_matrix, 
-  network, 
-  .source=arguments$options$source,
-  minsize = arguments$options$minsize,
-  verbose = verbose,
-  eset.filter = FALSE
-  )
 
 if (verbose) {
   sprintf("Exporting to %s...", arguments$args[2])
 }
 
-write_csv(tf_activity, arguments$args[2])
+df_progeny_score %>% write_csv(arguments$args[2])
+
