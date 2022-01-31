d7546de431a7dd40d2d862740c1663473b5848bb
Pablo R. Mier <pablo.rodriguez-mier@uni-heidelberg.de>
2022-01-31 09:42:37 +0100

    Add optional verbose argument to tfenrichment bb

--- building-blocks/core/src/tfenrichment/main.py
+++ building-blocks/core/src/tfenrichment/main.py
@@ -10,7 +10,7 @@ from permedcoe import FILE_OUT
 @container(engine="SINGULARITY", image="toolset.sif")
 @binary(binary="Rscript --vanilla /opt/tf_enrichment.R")
 @task(data=DIRECTORY_IN, result=FILE_OUT)
-def tf_enrichment(input_file=None, output_file=None, verbose='-v'):
+def tf_enrichment(input_file=None, output_file=None, verbose_flag='-v', verbose='T'):
     """
     The Definition is equal to:
         Rscript --vanilla /opt/tf_enrichment.R <de_input_file> <output_file>
@@ -19,5 +19,4 @@ def tf_enrichment(input_file=None, output_file=None, verbose='-v'):
 
 
 def invoke(input, output, config):
-    print(input, output)
-    tf_enrichment(input_file=input[0], output_file=output[0])
+    tf_enrichment(input_file=input[0], output_file=output[0], verbose=input[1])
